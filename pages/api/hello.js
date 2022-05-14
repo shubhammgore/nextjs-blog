554c150cabdb394fb1dd4d2b9c6caf0e2598a7be
shubham <shubh.shubham21@gmail.com>
2022-05-14 16:01:52 +0530

    first commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,4 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
+// req = HTTP incoming message, res = HTTP server response
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+	res.status(200).json({ text: 'Hello' });
 }
